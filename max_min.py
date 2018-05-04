da6c545d6d5fabbbd51aac46f4305e102a1a7482
raviteja482 <raviteja.teja89@gmail.com>
2018-05-03 22:24:45 -0400

    MY FIRST PYTHON PROGRAM

---
+++ max_min.py
@@ -0,0 +1,12 @@
+a = [15,26,78,54,69]
+
+print("Maximum element in the list is :", max(a), "\nMinimum element in the list is :", min(a))
+
+
+
+a = []
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    a.append(numbers)
+print("Maximum element in the list is :", max(a), "\nMinimum element in the list is :", min(a))
---
+++ reverse_string.py
@@ -0,0 +1,7 @@
+def reverse(string):
+    string = string[::-1]
+    return string
+ 
+s = "Keerthi"
+
+print (reverse(s))
